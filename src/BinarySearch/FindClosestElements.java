2819567de5f29d6d0e4a2a04a567bb1edd418b0f
shubham <shubhdgaikwad@gmail.com>
2022-04-16 18:50:28 +0530

    DSA/BinarySearch : Find closest elements

---
+++ src/BinarySearch/FindClosestElements.java
@@ -0,0 +1,34 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindClosestElements {
+
+    public static void main(String[] args) {
+        int[] nums = {10, 12, 15, 17, 18, 20, 25 };
+        int target = 16, k = 4;
+
+        System.out.println(findClosestElements(nums, k, target));
+    }
+
+    public static List<Integer> findClosestElements(int[] nums, int k, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (right - left >= k)
+        {
+            if (Math.abs(nums[left] - target) > Math.abs(nums[right] - target)) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return Arrays.stream(nums, left, right + 1).boxed()
+                .collect(Collectors.toList());
+    }
+}
