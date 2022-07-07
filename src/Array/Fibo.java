56f0222fcd8ea1df930f7f59e72fe5f00cd37ff8
shubham <shubhdgaikwad@gmail.com>
2022-07-07 11:32:12 +0530

    DSA/array : added array problems

---
+++ src/Array/Fibo.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Fibo {
+    public int fib(int n) {
+        if(n<=1)return n;
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        Fibo fibo = new Fibo();
+        System.out.println(fibo.fib(4));
+    }
+}
---
+++ src/Array/sortedSquares.java
@@ -0,0 +1,30 @@
+package Array;
+
+/*Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].*/
+public class sortedSquares {
+    public int[] sortedSquares(int[] nums) {
+
+        int n =nums.length;
+        int[] result = new int[n];
+        int li=0;
+        int ri=n-1;
+
+
+        for(int i=n-1;i>=0;i--){
+            if(Math.abs(nums[li]) > Math.abs(nums[ri])){
+                result[i]=nums[li]*nums[li];
+                li++;
+            }else{
+                result[i]=nums[ri]*nums[ri];
+                ri--;
+            }
+        }
+
+        return result;
+
+    }
+
+}
