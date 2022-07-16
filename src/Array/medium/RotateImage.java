c7f9577d4d476ab783222dd9667863706c3bb3e4
shubham <shubhdgaikwad@gmail.com>
2022-07-16 09:10:23 +0530

    DSA/array : modified rotate image.

--- src/Array/medium/RotateImage.java
+++ src/Array/medium/RotateImage.java
@@ -4,19 +4,20 @@ public class RotateImage {
 
     public void rotate(int[][] matrix) {
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){
+        int len = matrix.length;
+        for(int i=0;i<len;i++){
+            for(int j=0;j<1;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i] ;
                 matrix[j][i] =temp;
             }
         }
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length/2;j++){
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len/2;j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length-1-j] ;
-                matrix[i][matrix.length-1-j] =temp;
+                matrix[i][j] = matrix[i][len-1-j] ;
+                matrix[i][len-1-j] =temp;
             }
         }
     }
